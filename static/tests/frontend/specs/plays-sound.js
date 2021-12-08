62c8532a34c7ed28a7e34f986c9a93748ae051df
Mike Solomon <mike@wavr.so>
2021-12-08 15:53:28 +0100

    Adds tests

---
+++ static/tests/frontend/specs/plays-sound.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+describe("plays sound", function () {
+	// create a new pad before each test run
+	beforeEach(async function () {
+		await helper.aNewPad();
+	});
+
+
+	it("plays the sound", async function () {
+		const chrome$ = helper.padChrome$;
+
+		// get the play button and click it
+		const $playButton = chrome$(".buttonicon-play");
+		$playButton.click();
+	});
+
+});
